ae3d6dd3c0529b07c88d1d7e75d35e38ced9d816
Zhuohuan LI <zixia@zixia.net>
2016-07-24 23:14:25 +0800

    typescript ready!

--- bin/index.ts
+++ bin/index.ts
@@ -1,310 +1,305 @@
-///<reference path="../typings/index.d.ts"/>
+#!env ts-node
+'use strict'
 
-import Http = require('http')
-import WebSocket = require('ws')
-import url = require('url')
-import express = require('express')
+import * as http from 'http'
+import * as url from 'url'
+import * as express from 'express'
 
-import log = require('npmlog')
+import log = require('npmlog') // https://github.com/Microsoft/TypeScript/issues/6751
 log.level = 'verbose'
 log.level = 'silly'
 
-import Listag = require('./listag')
 
-const server = Http.createServer()
- , app = express()
-  // process.env.PORT is set by Heroku/Cloud9
-  var port = process.env.PORT || 8080
-
-server.on('request', app);
-server.listen(port, function () { console.log('Listening on ' + server.address().port) });
-
-// https://github.com/websockets/ws/blob/master/doc/ws.md
-const wss = new WebSocket.Server({
-  // host: process.env.IP
-  // , port: process.env.PORT
-  handleProtocols
-  , verifyClient
-  , server
-})
-
-const dbUri = process.env.MONGODB_URI
-
+const app = express()
 app.use(function (req, res) {
-  res.send(`
- <!DOCTYPE html>
- <html>
-  <head>
-  <meta charset="utf-8" />
-  <link rel="icon" type="image/png" href="wechaty-favicon-32x32.png" sizes="32x32" />
-  <link rel="icon" type="image/png" href="wechaty-favicon-16x16.png" sizes="16x16" />
-
-  <title>WebSocket Test</title>
-  <script language="javascript" type="text/javascript">
-
-  var token = prompt("Please enter your token", "wechaty")
-
-  var wsUri = "wss://echo.websocket.org/";
-  wsUri = document.location.href.replace(/^http(.?):/, 'ws$1:') + 'websocket/token/' + token
-
-  var output;
-
-  function init()
-  {
-    output = document.getElementById("output");
-    testWebSocket();
-  }
-
-  function testWebSocket()
-  {
-    websocket = new WebSocket(wsUri, 'web|0.0.1')
-    websocket.onopen = function(evt) { onOpen(evt) }
-    websocket.onclose = function(evt) { onClose(evt) }
-    websocket.onmessage = function(evt) { onMessage(evt) }
-    websocket.onerror = function(evt) { onError(evt) }
-  }
-
-  function onOpen(evt)
-  {
-    console.log(evt)
-    writeToScreen("CONNECTED");
-    doSend("WebSocket rocks");
-  }
-
-  function onClose(evt)
-  {
-    console.log(evt)
-    writeToScreen("DISCONNECTED");
-  }
-
-  function onMessage(evt)
-  {
-    console.log(evt)
-    writeToScreen('<span style="color: blue;">RESPONSE ALL: ' + JSON.stringify(evt) +'</span>');
-    writeToScreen('<span style="color: blue;">RESPONSE DATA: ' + evt.data+'</span>');
-    // websocket.close();
-  }
-
-  function onError(evt)
-  {
-    writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
-  }
-
-  function doSend(message)
-  {
-    writeToScreen("SENT: " + message);
-    websocket.send(message);
-  }
-
-  function writeToScreen(message)
-  {
-    var pre = document.createElement("p");
-    pre.style.wordWrap = "break-word";
-    pre.innerHTML = message;
-    output.appendChild(pre);
-  }
-
-  window.addEventListener("load", init, false);
-
-  </script>
-  </head>
-  <body>
-
-  <h2>Wechaty.io WebSocket Test</h2>
-
-  <div id="output"></div>
-
-  </body>
-</html>
-  `)
+  res.send(`<h1>Wechaty.io</h1>`)
 })
 
+const server = http.createServer()
+const port = process.env.PORT || 8080 // process.env.PORT is set by Heroku/Cloud9
 
+server.listen(port, _ => {
+  console.log('Listening on ' + server.address().port) 
+})
 
-function verifyClient(info, done) { // http://stackoverflow.com/a/19155613/1123955
-  console.log('verifyClient()')
+server.on('request', app)
 
-  const {origin, secure, req} = info
-  console.log(req.url)
+// const dbUri = process.env.MONGODB_URI
 
-  let token = authToken(req.headers.authorization)
-              || urlToken(req.url)
-
-  if (!token) {
-    return done(false, 400, 'Bad Request')
-  }
-
-  if (!auth(token)) {
-    console.log('auth fail')
-    return done(false, 401, 'Unauthorized')
-  }
-
-  req.user = token
-
-  done(true, 200, 'Ok')
-
-}
-
-function auth(token) {
-  console.log('token: ' + token)
-  return !!token
-}
-
-function urlToken(url) {
-  const matches = String(url).match(/token\/(.+)$/i)
-  return matches && matches[1]
-}
-
-function authToken(authorization) {
-  // https://github.com/KevinPHughes/ws-basic-auth-express/blob/master/index.js
-  if (!authorization) {
-    console.log('no authorization')
-    return null
-  }
-  const parts = authorization.split(' ')
-  if (parts.length !== 2) {
-    console.log('authorization part is not 2')
-    return null
-  }
-  const scheme = parts[0]
-  const token = parts[1]
-  if (!/Token/i.test(scheme) || !token) {
-    console.log('authorization schema is not Token')
-    return null
-  }
-  return token
-}
-
-function handleProtocols(protocols, done) {
-  // https://bugs.chromium.org/p/chromium/issues/detail?id=398407#c2
-  console.log('handleProtocols() protocols: ' + protocols)
-
-  done(true, protocols[0])
-}
-
+import { Listag } from 'listag'
 const ltSocks = new Listag()
 
-wss.on('connection', function connection(ws) {
-  log.verbose('Io', 'wss.on event connection')
-  //console.log(ws)
-  // console.log(': ' + ws.upgradeReq.client.user)
-//  upgradeReq.socket/connection/client
 
-  const user      = ws.upgradeReq.user
-  const protocol  = ws.protocol
 
-  console.log('user: ' + user)
-  console.log('protocol: ' + protocol)
+/**
+ * 
+ * IoSocket
+ * 
+ */
+import * as WebSocket from 'ws'
 
-  const [module, version] = protocol.split('|')
+class IoSocket {
+  wss: WebSocket.Server
 
-  Object.assign(ws, {
-    user, module
-  })
-  ltSocks.add(ws).tag({user, module})
-
-  switch(module.toLowerCase()) {
-    case 'web':
-      onWebConnection(ws)
-      break
-    case 'io':
-      onIoConnection(ws)
-      break
-    default:
-      log.error('Io', 'protocol module [%s] not supported', module)
-      break
-  }
-})
-
-function onWebConnection(ws) {
-  wsCast(ws.user, 'io', 'new visitor from web')
-  var location = url.parse(ws.upgradeReq.url, true);
-  // you might use location.query.access_token to authenticate or share sessions
-  // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
-
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    console.log('send dong')
-    const e = JSON.parse(message)
-    ws.send(JSON.stringify({
-      name: 'dong'
-      , data: e.data
-    }))
-  })
-  ws.on('error', gone.bind(ws))
-  ws.on('close', gone.bind(ws))
-
-  function gone(e) {
-    log.verbose('Io', 'onWebConnection() gone(%s)', e)
-
-    ltSocks.del(this)
-    this.close()
-
-    wsCast(this.user, 'io', 'connect lost')
+  constructor(
+    private server: http.Server
+    , private auth: (token: string) => Promise<string>
+    , private connect: (client: WebSocket) => void 
+  ) {
   }
 
-  ws.send('something from c9');
-  ws.send('welcome from zixia')
-  ws.send(JSON.stringify({
-    name: 'zixia'
-    , data: 'welcome from zixia 2'
-  }))
-}
+  init(): Promise<IoSocket> {
+    // https://github.com/websockets/ws/blob/master/doc/ws.md
+    const options = {
+      handleProtocols: this.handleProtocols
+      , verifyClient: this.verifyClient
+      , server: this.server
+      // , host: process.env.IP
+      // , port: process.env.PORT
+    }
+    this.wss = new WebSocket.Server(options)
+    this.wss.on('connection', this.connect)
 
-function onIoConnection(ws) {
-  log.verbose('Io', 'onIoConnection()')
-  wsCast(ws.user, 'web', 'new visitor from io')
+    return Promise.resolve(this)
+  }
 
-  ws
-  .on('message', function incoming(message) {
-    log.verbose('onIo', '%s, %s, recv msg: %s', message.length, typeof message, message)
+  /**
+   * https://bugs.chromium.org/p/chromium/issues/detail?id=398407#c2
+   */
+  private handleProtocols(protocols, done) {
+    console.log('handleProtocols() protocols: ' + protocols)
+    done(true, protocols[0])
+  }
 
-    wsCast(ws.user, 'web', message)
+  /**
+   * check token for websocket client
+   * http://stackoverflow.com/a/19155613/1123955
+   */
+  private verifyClient(
+    info: {
+      origin: string
+      secure: boolean
+      req: http.ServerRequest
+    }
+    , done: (res: boolean, code?: number, message?: string) => void
+  ): void {
+    log.verbose('WebSocket', 'verifyClient()')
 
-    console.log('reply roger')
-    ws.send('roger')
-  })
-  .on('error', e => {
-    console.log('error:' + e)
-    ltSocks.del(ws)
-    wsCast(ws.user, 'web', 'connection lost event[error]: ' + e)
-  })
-  .on('close', e => {
-    console.log('srv on close:' + e)
-    ws.close()
-    ltSocks.del(ws)
-    wsCast(ws.user, 'web', 'connection lost event[close]: ' + e)
-  })
+    const {origin, secure, req} = info
+    log.verbose('verifyClient()', req.url)
 
-  ws.send('something from srv')
+    const token = this.getToken(req)
 
-}
+    if (!token) {
+      return done(false, 400, 'Bad Request')
+    }
 
-function wsCast(user, module, msg) {
-  log.verbose('wsCast', '%s, %s, %s', user, module, msg)
+    this.auth(token)
+        .then(_ => {
+          req['user'] = token
+          done(true, 200, 'Ok')
+        })
+        .catch(e => {
+          console.log('auth fail')
+          return done(false, 401, 'Unauthorized')
+        })
+  }
 
-  const tagMap = {user, module}
-  console.log('tagMap')
-  console.log(tagMap)
-  const socks = ltSocks.get(tagMap)
+  private getToken(req: http.ServerRequest): string {
+    const token = authToken(req.headers.authorization)
+               || urlToken(req.url)
+    return token
 
-  console.log('ltSocks length: ' + (ltSocks && ltSocks.length))
-  // console.log('socks' + socks)
-  console.log('socks length: ' + (socks && socks.length))
+    /////////////////////////
 
-  ltSocks.forEach(v => {
-    // console.log('user: ' + v.user)
-    let tagMap = ltSocks.getTag(v)
-    console.log(tagMap)
-  })
+    function urlToken(url) {
+      const matches = String(url).match(/token\/(.+)$/i)
+      return matches && matches[1]
+    }
 
-  if (socks) {
-    log.verbose('wsCast', 'found %d socks', socks.length)
-    socks.forEach(s => {
-      if (s.readyState === WebSocket.OPEN) {
-        s.send(module + '[' + user + ']: ' + msg)
+    function authToken(authorization) {
+      // https://github.com/KevinPHughes/ws-basic-auth-express/blob/master/index.js
+      if (!authorization) {
+        console.log('no authorization')
+        return null
       }
-    })
+      const parts = authorization.split(' ')
+      if (parts.length !== 2) {
+        console.log('authorization part is not 2')
+        return null
+      }
+      const scheme = parts[0]
+      const token = parts[1]
+      if (!/Token/i.test(scheme) || !token) {
+        console.log('authorization schema is not Token')
+        return null
+      }
+      return token
+    }    
   }
 }
 
+
+/**
+ * Io Server
+ */
+class IoManager {
+  constructor() {
+
+  }
+
+  connect(client: WebSocket): void {
+    log.verbose('onConnect()')
+    log.verbose('Io', 'clients.on event connection')
+    // console.log(ws)
+    // console.log(': ' + ws.upgradeReq.client.user)
+  //  upgradeReq.socket/connection/client
+
+    const user      = client.upgradeReq['user']
+    const protocol  = client['protocol']
+
+    console.log('user: ' + user)
+    console.log('protocol: ' + protocol)
+
+    const [module, version] = protocol.split('|')
+
+    Object.assign(client, {
+      user
+      , module
+    })
+
+    ltSocks.add(client).tag({user, module})
+
+    switch (module.toLowerCase()) {
+      case 'web':
+        this.onWebConnection(client)
+        break
+      case 'io':
+        this.onIoConnection(client)
+        break
+      default:
+        log.error('Io', 'protocol module [%s] not supported', module)
+        break
+    }
+  }
+
+  onWebConnection(client: WebSocket) {
+    this.broadcast(client['user'], 'io', 'new visitor from web')
+    var location = url.parse(client.upgradeReq.url, true);
+    // you might use location.query.access_token to authenticate or share sessions
+    // or client.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+
+    client.on('message', function incoming(message) {
+      console.log('received: %s', message);
+      console.log('send dong')
+      const e = JSON.parse(message)
+      client.send(JSON.stringify({
+        name: 'dong'
+        , data: e.data
+      }))
+    })
+    client.on('error', gone.bind(client))
+    client.on('close', gone.bind(client))
+
+    function gone(e) {
+      log.verbose('Io', 'onWebConnection() gone(%s)', e)
+
+      ltSocks.del(this)
+      this.close()
+
+      this.broadcast(this.user, 'io', 'connect lost')
+    }
+
+    client.send('something from c9');
+    client.send('welcome from zixia')
+    client.send(JSON.stringify({
+      name: 'zixia'
+      , data: 'welcome from zixia 2'
+    }))
+  }
+
+  onIoConnection(client: WebSocket) {
+    log.verbose('Io', 'onIoConnection()')
+    this.broadcast(client['user'], 'web', 'new visitor from io')
+
+    client
+    .on('message', function incoming(message) {
+      log.verbose('onIo', '%s, %s, recv msg: %s', message.length, typeof message, message)
+
+      this.broadcast(client['user'], 'web', message)
+
+      console.log('reply roger')
+      client.send('roger')
+    })
+    .on('error', e => {
+      console.log('error:' + e)
+      ltSocks.del(client)
+      this.broadcast(client['user'], 'web', 'connection lost event[error]: ' + e)
+    })
+    .on('close', e => {
+      console.log('srv on close:' + e)
+      client.close()
+      ltSocks.del(client)
+      this.broadcast(client['user'], 'web', 'connection lost event[close]: ' + e)
+    })
+
+    client.send('something from srv')
+
+  }
+
+  broadcast(user, module, msg) {
+    log.verbose('broadcast', '%s, %s, %s', user, module, msg)
+
+    const tagMap = {user, module}
+    console.log('tagMap')
+    console.log(tagMap)
+    const socks = ltSocks.get(tagMap)
+
+    console.log('ltSocks length: ' + (ltSocks && ltSocks.length))
+    // console.log('socks' + socks)
+    console.log('socks length: ' + (socks && socks.length))
+
+    ltSocks.forEach(v => {
+      // console.log('user: ' + v.user)
+      let tagMap = ltSocks.getTag(v)
+      console.log(tagMap)
+    })
+
+    if (socks) {
+      log.verbose('broadcast', 'found %d socks', socks.length)
+      socks.forEach(s => {
+        if (s.readyState === WebSocket.OPEN) {
+          s.send(module + '[' + user + ']: ' + msg)
+        }
+      })
+    }
+  }
+
+}
+
+/**
+ * Auth Class
+ */
+class IoAuth {
+  constructor() {
+
+  }
+
+  auth(token: string): Promise<string | void> {
+    if (token) {
+      return Promise.resolve(token)
+    }
+    return Promise.reject(false)
+  }
+
+}
+
+const ioManager = new IoManager()
+const ioAuth = new IoAuth()
+
+const ioSocket = new IoSocket(
+  server
+  , ioAuth.auth.bind(ioAuth)
+  , ioManager.connect.bind(ioManager)
+)
+ioSocket.init()
--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,8 +1,8 @@
 'use strict'
 
-import { test } from 'tap'
+import { test } from 'tape'
 
 test('Listag smoking test', t => {
   t.equal(1, 1, 'should be equal')
-
+  t.end()
 })
